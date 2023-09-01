74523aedb98a8913eca86d943db05c4afc0b7aa5
unknown <zulfikarmuhamad432@gmail.com>
2023-09-01 15:47:28 +0700

    update

--- src/components/projects/projects.components.jsx
+++ src/components/projects/projects.components.jsx
@@ -4,43 +4,52 @@ import { FaGithub, FaGlobe } from 'react-icons/fa';
 import Button from '../button/button.component';
 
 function Projects(props) {
-  return (
-    <section id="projects">
-      <h1>My Projects</h1>
-      <div className="projects-list">
-        {projects.map((project, index) => (
-          <div key={index} className="project-card" data-aos="zoom-in-up">
-            <div className="project-preview">
-              <img src={project.preview} alt="project preview image" />
-            </div>
-            <div className="project-body">
-              <h3>{project.title}</h3>
-              <p>{project.desc}</p>
-              <div className="tags">
-                {project.tags.map((tag) => (
-                  <div key={tag}>{tag}</div>
+    return (
+        <section id="projects">
+            <h1>My Projects</h1>
+            <div className="projects-list">
+                {projects.map((project, index) => (
+                    <div
+                        key={index}
+                        className="project-card"
+                        data-aos="zoom-in-up"
+                    >
+                        <div className="project-preview">
+                            <img
+                                src={project.preview}
+                                alt="project preview image"
+                            />
+                        </div>
+                        <div className="project-body">
+                            <h3>{project.title}</h3>
+                            <p>{project.desc}</p>
+                            <div className="tags">
+                                {project.tags.map((tag) => (
+                                    <div key={tag}>{tag}</div>
+                                ))}
+                            </div>
+                            <div className="buttons">
+                                <a href={project.github} target="_blank">
+                                    <Button buttonType="dark">
+                                        Github Repo
+                                        <FaGithub />
+                                    </Button>
+                                </a>
+                                {project.siteLink && (
+                                    <a href={project.siteLink} target="_blank">
+                                        <Button buttonType="light">
+                                            Live Demo
+                                            <FaGlobe />
+                                        </Button>
+                                    </a>
+                                )}
+                            </div>
+                        </div>
+                    </div>
                 ))}
-              </div>
-              <div className="buttons">
-                <a href={project.github} target="_blank">
-                  <Button buttonType="dark">
-                    Github Repo
-                    <FaGithub />
-                  </Button>
-                </a>
-                <a href={project.siteLink} target="_blank">
-                  <Button buttonType="light">
-                    Live Demo
-                    <FaGlobe />
-                  </Button>
-                </a>
-              </div>
             </div>
-          </div>
-        ))}
-      </div>
-    </section>
-  );
+        </section>
+    );
 }
 
 export default Projects;
--- src/lib/data.js
+++ src/lib/data.js
@@ -1,91 +1,90 @@
 export const links = [
-  {
-    label: 'Home',
-    hash: '#home'
-  },
-  {
-    label: 'About',
-    hash: '#about'
-  },
-  {
-    label: 'Skills',
-    hash: '#skills'
-  },
-  {
-    label: 'Projects',
-    hash: '#projects'
-  },
+    {
+        label: 'Home',
+        hash: '#home'
+    },
+    {
+        label: 'About',
+        hash: '#about'
+    },
+    {
+        label: 'Skills',
+        hash: '#skills'
+    },
+    {
+        label: 'Projects',
+        hash: '#projects'
+    },
 
-  // {
-  //   label: 'Experience',
-  //   hash: '#experience'
-  // },
-  {
-    label: 'Contact',
-    hash: '#contact'
-  }
+    // {
+    //   label: 'Experience',
+    //   hash: '#experience'
+    // },
+    {
+        label: 'Contact',
+        hash: '#contact'
+    }
 ];
 
 export const skills = [
-  'HTML',
-  'CSS',
-  'TailwindCSS',
-  'Javascript',
-  'ReactJs',
-  'VueJs',
-  'NodeJs',
-  'ExpressJs',
-  'MySQL',
-  'Git'
+    'HTML',
+    'CSS',
+    'TailwindCSS',
+    'Javascript',
+    'ReactJs',
+    'VueJs',
+    'NodeJs',
+    'ExpressJs',
+    'MySQL',
+    'Git'
 ];
 
 export const projects = [
-  {
-    title: 'Space tourism multi-page website',
-    desc: 'This project is a fun collaboration with Scrimba and Kevin Powell! If you like, you can follow along and watch Kevin complete the project on Scrimba. Lots to learn!',
-    preview:
-      'https://res.cloudinary.com/dz209s6jk/image/upload/f_auto,q_auto,w_700/Challenges/x8skdsukkmwiwxejthio.jpg',
-    tags: ['ReactJS', 'scss'],
-    siteLink: 'https://space-tourism-web-nu.vercel.app/',
-    github: 'https://github.com/zulfikarm321/space-tourism-web'
-  },
-  {
-    title: 'E-commerce product page',
-    desc: "In this challenge, you'll build a beautiful product page. We'll be putting your JS skills to the test with a lightbox product gallery and cart functionality!",
-    preview:
-      'https://res.cloudinary.com/dz209s6jk/image/upload/f_auto,q_auto,w_700/Challenges/uehz7kwoo7gesfebbqi8.jpg',
-    tags: ['ReactJS', 'scss'],
-    siteLink:
-      'https://zulfikarm321.github.io/e-commerce-product-page-frontend-mentor/',
-    github:
-      'https://github.com/zulfikarm321/e-commerce-product-page-frontend-mentor'
-  },
-  {
-    title: 'Rest Countries',
-    desc: "If you're wanting to test your JavaScript skills this is the challenge for you. Use whichever JS framework you prefer and pull data from the REST Countries API.",
-    preview:
-      'https://res.cloudinary.com/dz209s6jk/image/upload/f_auto,q_auto,w_700/Challenges/wirxeocmd6tpnn9c5oqc.jpg',
-    tags: ['VanillaJS', 'css', 'API'],
-    siteLink:
-      'https://zulfikarm321.github.io/Rest-countries-API-Frontend-Mentor/',
-    github: 'https://github.com/zulfikarm321/Rest-countries-API-Frontend-Mentor'
-  },
-  {
-    title: 'Todo app',
-    desc: 'The classic todo app with a few twists! This app includes a dark/light theme toggle and drag & drop reordering for anyone wanting an extra test.',
-    preview:
-      'https://res.cloudinary.com/dz209s6jk/image/upload/f_auto,q_auto,w_700/Challenges/llcq9eiv3ney5tkxgdtu.jpg',
-    tags: ['VueJS', 'tailwindcss', 'VueX', 'localstorage'],
-    siteLink: 'https://todo-app-git-main-zulfikarm321.vercel.app/',
-    github: 'https://github.com/zulfikarm321/todo-app'
-  },
-  {
-    title: 'Interactive comments section',
-    desc: "This is project will put your JavaScript skills to the test. We provide a JSON file to pull the data, but it's also a perfect project to build as a full-stack CRUD app!",
-    preview:
-      'https://res.cloudinary.com/dz209s6jk/image/upload/f_auto,q_auto,w_700/Challenges/v9wskl4mnbt5gbxm4o2r.jpg',
-    tags: ['VueJS', 'tailwindcss', 'VueX', 'localstorage'],
-    siteLink: 'https://interactive-comment-section-eosin.vercel.app/',
-    github: 'https://github.com/zulfikarm321/interactive-comment-section'
-  }
+    {
+        title: 'Valopedia',
+        desc: 'valopedia is a simple website that displays information from the game valorant. I made this website because I was learning next js',
+        preview:
+            '/screencapture-valopedia-kappa-vercel-app-22697a3d-45bf-8dd7-4fec-84a9e28c69d7-2023-08-31-14_58_24.webp',
+        tags: ['Next-Typescript', , 'TailwindCSS'],
+        siteLink: 'https://valopedia-kappa.vercel.app/',
+        github: 'https://github.com/zulfikarm321/valopedia'
+    },
+    {
+        title: 'Ragam Artikel',
+        desc: 'This is my project that we made from the results of learning at the 2023 Alkademi Pesilat Bootcamp. Here we learn how to collaborate with the team',
+        preview: '/ss-dashboard-artikel.webp',
+        tags: ['NuxtJs', , 'TailwindCSS', 'MySQL', 'ExpressJs'],
+        siteLink: '',
+        github: 'https://github.com/kelompokpesilat1/frontend/tree/dev'
+    },
+
+    {
+        title: 'Space tourism multi-page website',
+        desc: "This is a project from a challenge on Mentor's frontend platform. from this project I learned to use react and react-router",
+        preview:
+            'https://res.cloudinary.com/dz209s6jk/image/upload/f_auto,q_auto,w_700/Challenges/x8skdsukkmwiwxejthio.jpg',
+        tags: ['ReactJS', 'scss'],
+        siteLink: 'https://space-tourism-web-nu.vercel.app/',
+        github: 'https://github.com/zulfikarm321/space-tourism-web'
+    },
+
+    {
+        title: 'Rest Countries',
+        desc: "If you're wanting to test your JavaScript skills this is the challenge for you. Use whichever JS framework you prefer and pull data from the REST Countries API.",
+        preview:
+            'https://res.cloudinary.com/dz209s6jk/image/upload/f_auto,q_auto,w_700/Challenges/wirxeocmd6tpnn9c5oqc.jpg',
+        tags: ['VanillaJS', 'css', 'API'],
+        siteLink:
+            'https://zulfikarm321.github.io/Rest-countries-API-Frontend-Mentor/',
+        github: 'https://github.com/zulfikarm321/Rest-countries-API-Frontend-Mentor'
+    },
+    {
+        title: 'Todo app',
+        desc: 'The classic todo app with a few twists! This app includes a dark/light theme toggle and drag & drop reordering for anyone wanting an extra test.',
+        preview:
+            'https://res.cloudinary.com/dz209s6jk/image/upload/f_auto,q_auto,w_700/Challenges/llcq9eiv3ney5tkxgdtu.jpg',
+        tags: ['VueJS', 'tailwindcss', 'VueX', 'localstorage'],
+        siteLink: 'https://todo-app-git-main-zulfikarm321.vercel.app/',
+        github: 'https://github.com/zulfikarm321/todo-app'
+    }
 ];
